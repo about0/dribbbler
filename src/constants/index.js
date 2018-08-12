689ac81daf308316e10f60871d45a3ef36661237
Bogdan Kostrytskyi <ghostautomatic@gmail.com>
2018-08-12 18:24:36 +0300

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const dribble = {
+  clientID: '237486fba3f6c8054f1df03ff45ef118fe17f6bf2d96f95d5ec2b32bb1f493d4',
+  clientSecret:
+    '051c6bc59622def003db139f2673ddf13f333912e9a5855e6574e8e716b91959'
+}
